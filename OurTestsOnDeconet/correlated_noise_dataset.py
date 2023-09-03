ad9a02bc72fa4c522a3a54fcc52daeb9dbe8ec68
ITAY GEVA <ITAYGE@rafael.co.il>
2023-09-03 09:30:17 +0300

    first test

---
+++ OurTestsOnDeconet/correlated_noise_dataset.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn
+from torch.utils.data import Dataset
+import numpy as np
+from torchvision.transforms import ToTensor
+
+
+def generate_highly_correlated_covariance_matrix(num_variables):
+    # Create a random covariance matrix with diagonal elements as 1 (variances)
+    covariance_matrix = 1 * torch.eye(num_variables)
+
+    # Set off-diagonal elements (covariances) to a positive constant
+    constant_covariance = 0.9
+
+    for i in range(num_variables):
+        for j in range(i + 1, num_variables):
+            covariance_matrix[i, j] = constant_covariance
+            covariance_matrix[j, i] = constant_covariance
+
+    return covariance_matrix
+
+
+class Synthetic(Dataset):
+    def __init__(self, input_vec) -> None:
+        self.input_vec = input_vec.t()
+
+    def __len__(self):
+        return self.input_vec.shape[0]
+
+    def __getitem__(self, index):
+        x = self.input_vec[index]
+
+        return x
+
+
+def synthetic_generator(input_dim, n_sensors, set_size):
+    noise = torch.normal(0, 0.01, size=(input_dim, n_sensors, set_size))
+    cov_matrix = generate_highly_correlated_covariance_matrix(n_sensors)
+    cholesky_matrix = torch.cholesky(cov_matrix, upper=False)
+    print(cholesky_matrix)
+    correlated_noise = torch.matmul(cholesky_matrix, noise)
+    input_vectors = correlated_noise.view(input_dim*n_sensors, set_size)
+
+    return input_vectors
+
+
+if __name__ == "__main__":
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    a = synthetic_generator(100, 3, 1000)
+    print(a)
