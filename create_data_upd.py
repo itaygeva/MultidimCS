3460317f47e9c1176aff549b4ef2c444fb89ac46
noa-raifler <55491297+noa-raifler@users.noreply.github.com>
2024-08-09 22:42:52 +0300

    updated to our data
    additional work: we need to calculate loss with respect to C (after the train..), play with param and statistics...

--- create_data_upd.py
+++ create_data_upd.py
@@ -1,74 +1,92 @@
 
 import numpy as np
+import torch
+import torch.nn
+from torch.utils.data import Dataset
+from torchvision.transforms import ToTensor
 
 
-def generate_brownian_noise(duration=1, fs=400, center=1):
+class Synthetic(Dataset):
+    def __init__(self, input_vec) -> None:
+
+        self.input_vec = input_vec.t()
+
+    def __len__(self):
+
+        return self.input_vec.shape[0]
+
+    def __getitem__(self, index):
+        x = self.input_vec[index]
+
+        return x
+
+
+def generate_brownian_noise(duration=1, fs=400, center=1): #sec,Hz,mean of BN
     """
     Generate Brownian noise (red noise) centered around a specified value.
-
-    :param duration: Length of the noise signal in seconds.
-    :param fs: Sampling frequency in Hz.
-    :param center: The center value around which the noise oscillates.
-    :return: NumPy array containing Brownian noise centered at the given value.
     we want our vector u to be with 400 elements (like the real Rafael's data), so dur*fs=400
     """
     # Generate white noise
     white_noise = np.random.normal(size=int(duration * fs))
-
     # Integrate the white noise to create Brownian noise
     brownian_noise = np.cumsum(white_noise)
     brownian_noise = np.interp(brownian_noise, (brownian_noise.min(), brownian_noise.max()),
                                (center - 1, center + 1))
-
     return brownian_noise
 
 class Data:
+    def __init__(self,rows,cols,sig1vt_supp,k_sparse):
+        self.rows=rows
+        self.cols=cols
+        self.sig1vt_supp=sig1vt_supp
+        self.k_sparse=k_sparse
 
-    def create_X(self,rows,cols, X_c_energy = 0.7, noise_energy=1e-6): #rows=400, cols=13, rows*cols
-        """ x_400*13 = X_c + X_inv + epsilon
-        X_c_energy:  the percentage of energy that the common holds. (0.7 for example..)
-        """
-        full_energy = 1
-        lambda1= full_energy * X_c_energy
-        u1= np.random.randn(rows,1)
-        v1=np.random.randn(1,cols)
-        X_c = lambda1 * np.dot(u1,v1)
 
-        X_inv =self.create_inovation(rows,cols)
-        epsilon = np.random.randn(rows,cols) * full_energy * noise_energy
+    def create_X_C_Z(self): #rows=400, cols=13, rows*cols
+        self.Ct=self.create_Ct()
+        self.Zt=self.create_Zt()
+        X=self.Ct+self.Zt
+        U, S, Vt = np.linalg.svd(X, full_matrices=True) #U_rxr, S_1xmin(r,c), Vt_cxc
+        min_rc=min(self.rows, self.cols)
+        Sigma = np.zeros((U.shape[1], Vt.shape[0]))
+        first_s=np.zeros((U.shape[1], Vt.shape[0]))
+        np.fill_diagonal(Sigma, S)
+        np.fill_diagonal(first_s, np.concatenate((S[0], np.zeros((1,min_rc-1))), axis=None))
+        C=U @ first_s @ Vt
+        Z=X-C
+        return X,C,Z
 
-        X = X_c + X_inv +epsilon
-        return X
 
     def create_Ct(self):
-        ut=generate_brownian_noise(duration=1, fs=400, center=1) #ut = u tag:)
-        sig1vt=np.random.uniform(-10, 10, 13)
+        ut=generate_brownian_noise(duration=1, fs=self.rows, center=1) #ut = u tag:)
+        sig1vt=np.random.uniform(-self.sig1vt_supp, self.sig1vt_supp, self.cols)
+        Ct=np.reshape(ut,(ut.shape[0],1)) @ np.reshape(sig1vt, (1,sig1vt.shape[0]))
+        return Ct
+
+
+    def create_Zt(self):
+        mu=0
+        sigma=1
+        matrix = np.random.normal(mu, sigma, (self.rows, self.cols))
+        k_col_rand=np.random.choice(self.cols, self.k_sparse, replace=True)
+        k_row_rand = np.random.choice(self.rows, self.k_sparse, replace=True)
+        matrix[k_row_rand,k_col_rand]=0
+        Zt=matrix
+        return Zt
+
+    def create_Dataset(self,num):
+        X_dataset=np.zeros((num,self.rows,self.cols))
+        C_dataset = np.zeros((num, self.rows, self.cols))
+        Z_dataset = np.zeros((num, self.rows, self.cols))
+        for i in range(num):
+            X,C,Z=self.create_X_C_Z()
+            X_dataset[i,:,:] = X
+            C_dataset[i, :, :] = C
+            Z_dataset[i, :, :] = Z
+        return X_dataset,C_dataset,Z_dataset
 
 
 
-    def create_Zt(self,rows,cols):
-        t= np.arange(0,rows)/ rows
-        angels_deg = np.array([0,30,60]).reshape(1,-1) #up to us.. cos(alp1*t) + cos(alp2*t) + ...
-        angels_rad = np.deg2rad(angels_deg)
-
-        cos_function = np.cos(np.sum(t.reshape(-1,1) * angels_rad, axis=1))
-        fft_cos = np.fft.fft(cos_function).reshape(-1,1)
-
-        alphas = np.random.randn(1,cols) * 10 #10? whatever we want
-        X_inv = fft_cos * alphas
-
-        return X_inv
-
-
-
-
-# data=Data()
-# X = data.create_data(400,13)
-# print('bla')
-
-# a = 8 + np.random.randn(400,13)
-# U,S,Vh = np.linalg.svd(a,full_matrices=False)
-# print(U.shape, S.shape, Vh.shape)
-# smat = np.diag(S)
-# sv=np.dot(smat,Vh)
-# print('bla')
+# data=Data(rows=400,cols=13,sig1vt_supp=10,k_sparse=200)
+# X,C,Z=data.create_X_C_Z()
+# print(X,C,Z)
